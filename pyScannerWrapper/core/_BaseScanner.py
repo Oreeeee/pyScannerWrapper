062f5fa35beed0b4172c629e785c3d967c2f5331
Oreeeee <oree3ee@gmail.com>
2023-07-14 21:00:31 +0200

    Comment the code

--- pyScannerWrapper/core/_BaseScanner.py
+++ pyScannerWrapper/core/_BaseScanner.py
@@ -2,7 +2,7 @@ from .structs import ScanResults
 
 
 class BaseScanner:
-    # Public
+    # Public variables
     input_ip_list: list
     input_port_list: list
     scanner_args: str
@@ -10,15 +10,22 @@ class BaseScanner:
     results: ScanResults = None
     running: bool = False
 
-    # Private
+    # Private variables
     __scanner_name: str
     __default_args: str
     __should_yield: bool
 
+    # Public methods
     def __init__(self):
         pass
 
     def scan(self) -> None:
+        """
+        This method will scan with the provided scanner.
+        It doesn't return anything, and will instead set the results property of the class.
+        If yielding is enabled, it will in addition yield the IP:Port of the server if a online target is found.
+        It is up to the inheriting class to override this method.
+        """
         pass
 
     def scan_yielder(self) -> None:
