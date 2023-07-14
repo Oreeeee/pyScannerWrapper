5fd69184fa0e363d7eaf1b77cb78b2fe5022b089
Oreeeee <oree3ee@gmail.com>
2023-07-14 21:46:11 +0200

    Initialize results in Masscan

--- pyScannerWrapper/scanners/_Masscan.py
+++ pyScannerWrapper/scanners/_Masscan.py
@@ -1,4 +1,7 @@
 from pyScannerWrapper.core import BaseScanner
+from pyScannerWrapper.core.structs import ScanResults
+import subprocess
+import threading
 
 
 class Masscan(BaseScanner):
@@ -8,4 +11,12 @@ class Masscan(BaseScanner):
         self.scanner_path_verify()
 
     def scan(self) -> None:
-        pass
+        # Initialize results
+        self.results = ScanResults(
+            scanner_name=self.scanner_name,
+            start_time=None,
+            end_time=None,
+            time_took=None,
+            args="",
+            results=[],
+        )
