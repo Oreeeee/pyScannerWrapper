9c6b93606b2cb829802f3337aa17cde757f1a425
Oreeeee <oree3ee@gmail.com>
2023-07-14 20:55:06 +0200

    Add Masscan class

---
+++ pyScannerWrapper/scanners/_Masscan.py
@@ -0,0 +1,8 @@
+from pyScannerWrapper.core import BaseScanner
+
+
+class Masscan(BaseScanner):
+    __scanner_name = "masscan"
+
+    def scan(self) -> None:
+        pass
---
+++ pyScannerWrapper/scanners/__init__.py
@@ -0,0 +1,1 @@
+from ._Masscan import Masscan
